b000c669f40b85f28332eadaccc479132058b5dc
Richard Luke Dillman <dillman85@gmail.com>
2011-05-24 15:44:03 -0700

    omniauth facebook connect / need to change for production

--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -1,6 +1,4 @@
 class Authentication < ActiveRecord::Base
   attr_accessible :user_id, :provider, :uid
   belongs_to :user
-  validates_presence_of :user_id, :uid, :provider
-  validates_uniqueness_of :uid, :scope => :provider
 end
