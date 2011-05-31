7ec44755a8536c7be64dc96e35f7c3df12f51f4a
Peter O'Braidan <pspetroff@gmail.com>
2011-05-31 15:18:24 -0700

    cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,6 @@
 set :application, "tcktcrckt.com"
 
+default_run_options[:pty] = true  # Must be set for the password prompt from git to work
 set :scm, :git
 set :repository,  "git@github.com:rdillman/tcktcrckt.git"
 set :deploy_via, :copy
