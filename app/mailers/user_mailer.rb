7bee217b23751888188164df4ecb605d64496f26
Peter O'Braidan <pspetroff@gmail.com>
2011-06-07 17:20:49 -0700

    cc

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,7 +5,7 @@ class UserMailer < ActionMailer::Base
     if alert
       @user= User.find(alert.user_id)
       @alarm_message = "Warning! Your location, "<<alert.location<<" will be swept at"<<alert.clean_time<<". - tcktcrckt"
-      mail(:to =>"<#{@user.text_address}>", :subject => "TicketCricket Alert!")
+      mail(:to =>"2565414750@txt.att.net", :subject => "TicketCricket Alert!")
     else
       return nil
     end
