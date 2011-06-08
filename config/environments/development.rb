03b060db19b171b586d9573bb8a6fbc6d4cc22c3
Peter O'Braidan <pspetroff@gmail.com>
2011-06-08 16:33:52 -0700

    email testin

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,13 +25,9 @@ Tcktcrckt::Application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :domain               => "gmail.com",
-    :user_name            => "streetsweeper.sf",
-    :password             => "fuckthep0l1c3",
-    :authentication       => "plain",
-    :enable_starttls_auto => true
+    :address              => 'localhost',
+    :port                 => 25,
+    :domain               => 'www.tcktcrckt.com',
   }
 
   # Print deprecation notices to the Rails logger
