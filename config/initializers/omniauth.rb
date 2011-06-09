23b7ce70f7df924a5786bf722014c613c931ff75
Richard Luke Dillman <dillman85@gmail.com>
2011-06-08 21:36:55 -0700

    omniauth add

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   # provider :twitter, 'CONSUMER_KEY', 'CONSUMER_SECRET'
-  provider :facebook, '125474050867821', '62316e062a23d81a5b0bfe25a15bc988'
+  provider :facebook, '125474050867821', '62316e062a23d81a5b0bfe25a15bc988', {:scope => 'email, offline_access', :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
   # provider :linked_in, 'CONSUMER_KEY', 'CONSUMER_SECRET'
 end
