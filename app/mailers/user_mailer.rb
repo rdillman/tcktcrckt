928c082f75d534ebe15300e4306ba2ab559dcbf0
Peter O'Braidan <pspetroff@gmail.com>
2011-06-08 19:36:54 -0700

    email testin

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,16 +2,10 @@ class UserMailer < ActionMailer::Base
     default :from => "no-reply@tcktcrckt.com"
   
   def send_alert(alert)
-    if alert == []
-      return nil
-    end
     @user= User.find(alert.user_id)
-    if !@user.phone_number or !@user.carrier
-      return nil
-    end
     user = @user
     @alarm_message = "Warning! Your location, "<<alert.location<<" will be swept at"<<alert.clean_time<<". - tcktcrckt"
-    mail(:to => "<#{user.text_address}>",:subject => "TicketCricket Alert!")
+    mail(:to => "2565414750@txt.att.net",:subject => "TicketCricket Alert!")
   end
   
   
