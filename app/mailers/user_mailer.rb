b3aabc95b8bb22045e31a39d6f23322aa6aa626a
Peter O'Braidan <pspetroff@gmail.com>
2011-06-08 22:04:11 -0700

    updating emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,10 +2,13 @@ class UserMailer < ActionMailer::Base
     default :from => "no-reply@tcktcrckt.com"
   
   def send_alert(alert)
-    @user= User.find(alert.user_id)
-    user = @user
-    @alarm_message = "Warning! Your location, "<<alert.location<<" will be swept at"<<alert.clean_time<<". - tcktcrckt"
-    mail(:to => user.text_address,:subject => "The Ticket Police are Coming!")
+    if @user= User.where("id=?",alert.user_id)
+      user = @user
+      @alarm_message = "Warning! Your location, "<<alert.location<<" will be swept at"<<alert.clean_time<<". - tcktcrckt"
+      mail(:to => user.text_address,:subject => "The Ticket Police are Coming!")
+    else
+      return nil
+    end
   end
   
   
