fa958343b6cc5256b166025ec960183cb8a7cb75
joseles de la cruz <joseles@gmail.com>
2011-07-01 01:19:24 -0700

    i18n so the mobile i18n works

--- app/controllers/lookup_controller.rb
+++ app/controllers/lookup_controller.rb
@@ -87,12 +87,12 @@ class LookupController < ApplicationController
         send = ct - 1.hour
         alert.update_attribute(:send_time, send.strftime("%B %e %Y at %H:%M"))
         alert.update_attribute(:nb4, false)
-        @message ="One Hour Before"
+        @message =I18n.translate('alert_controller.alert_messages.options.1_hour')
       else
         nb4_time = ct-1.day + (19 - ct.hour).hour
         alert.update_attribute(:send_time, nb4_time.strftime("%B %e %Y at %H:%M"))
         alert.update_attribute(:nb4, true)
-        @message ="Night Before"
+        @message =I18n.translate('alert_controller.alert_messages.options.night_before')
       end
       respond_to do |format|
         format.html 
